66abee2bec8c1ebdb2b716113555e55cf611d8b0
Naz Khan <nazmuskhan@yahoo.com>
2017-05-26 16:43:56 -0400

    starting to build components

---
+++ expense-tracker-client/src/components/Main.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+export default class Main extends React.Component {
+  render() {
+    <NavBar />
+  }
+}
---
+++ expense-tracker-client/src/components/NavBar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+class NavBar extends React.Component {
+  constructor(){
+    super()
+  }
+}
